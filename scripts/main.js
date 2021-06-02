bb187cd3dbe30b2e2169caa7b020d61de82e85f9
PYJS123 <bibi.abrah@gmail.com>
2021-06-02 16:32:18 +0100

    Checkpoint

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,19 @@
+const url = 'http://worldtimeapi.org/api/timezone/';
 async function getTime(place) {
-  time =  await fetch(place);
+  const response = await fetch(url+place);
+  const data = await response.json();
+  return data;
 }
+
+async function writeData() {
+  const elems = document.getElementsByClassName('panel');
+  for (let i = 0; i < elems.length; i++) {
+    const name = elems[i].innerHTML;
+    const cities = await fetch(url);
+    // let i = 0;
+    // while (i < )
+    console.log(cities);
+  }
+}
+
+writeData()
